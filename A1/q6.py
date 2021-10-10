21269e42da659958d49897a3fdadaa5ae2739474
Hassan Khan <hassanjkhan6@gmail.com>
2021-10-10 13:23:38 -0400

    Basically finished q6 just got alittle work left for combining the recommended into one big list

---
+++ A1/q6.py
@@ -0,0 +1,38 @@
+import fileinput
+import pandas as pd
+import sys
+
+transactions = sys.argv[1].split(",")
+recommended = []
+
+print("transactions below:")
+print(transactions)
+
+def contains(transactions, Antecedents):
+    for each in Antecedents:
+        if each not in transactions:
+            return False
+    return True
+
+# open file in read mode
+fileName = "walmart_rules.csv"
+for line in pd.read_csv(fileName,chunksize=1):
+    temp = line['Antecedent'].to_string().split("'")
+    Antecedents = []
+    for i in range(0,len(temp)):
+        if(i%2!=0):
+            Antecedents.append(temp[i])
+
+    temp_Consequent = line['Consequent'].to_string().split("'")
+    Consequent = []
+    for i in range(0,len(temp_Consequent)):
+        if(i%2!=0):
+            Consequent.append(temp_Consequent[i])
+
+    # check if every Antecedent is in transaction list
+    # if yes then add the Consequent to recommended else do not add
+    if (contains(transactions,Antecedents)):
+        recommended.append(Consequent)
+
+
+print(recommended)
